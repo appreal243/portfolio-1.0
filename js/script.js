f87fd475b1149b1104b5efeac48450c1fd46ffb8
appreal243 <apprgame243@gmail.com>
2024-04-01 21:20:24 +0700

    upload portfolio-1.0

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// --------------- Section active links ---------------
+// Mendapatkan semua elemen <section>
+const sections = document.querySelectorAll("section");
+
+// Mendapatkan semua tautan navigasi di dalam elemen <nav> di dalam <header>
+const navLinks = document.querySelectorAll("header nav a");
+
+// Fungsi untuk menentukan tautan navigasi yang aktif
+function setActiveNavLink() {
+  const offset = window.innerHeight * 0.5; // Offset tambahan setengah tinggi jendela browser
+
+  // Loop melalui setiap elemen <section>
+  sections.forEach((section) => {
+    // Mendapatkan posisi atas dan bawah dari setiap elemen <section> dengan offset tambahan
+    const top = section.offsetTop - offset;
+    const bottom = top + section.offsetHeight;
+
+    // Mendapatkan id dari setiap elemen <section>
+    const id = section.getAttribute("id");
+
+    // Memeriksa apakah posisi scroll saat ini berada di dalam elemen <section>
+    if (window.scrollY >= top && window.scrollY < bottom) {
+      // Menghapus kelas 'active' dari semua tautan navigasi
+      navLinks.forEach((link) => link.classList.remove("active"));
+
+      // Menambahkan kelas 'active' ke tautan navigasi yang sesuai dengan elemen <section> yang sedang dilihat
+      document
+        .querySelector(`header nav a[href="#${id}"]`)
+        .classList.add("active");
+    }
+
+    // --------------- Stiky header ---------------
+    let header = document.querySelector("header");
+    header.classList.toggle("stiky", window.scrollY > 100);
+    
+    // --------------- Remove Sidebar anda Icon Close ---------------
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+  });
+}
+
+// Menjalankan fungsi setActiveNavLink saat terjadi peristiwa scroll
+window.addEventListener("scroll", setActiveNavLink);
+
+// Menjalankan fungsi setActiveNavLink saat halaman dimuat
+document.addEventListener("DOMContentLoaded", setActiveNavLink);
+
+// --------------- Slide Sidebar and Icon close ---------------
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
